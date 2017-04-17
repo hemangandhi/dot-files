bf7e8c30f63f20ea3ae3745bc5f340919a722a48
Heman Gandhi <hemangandhi@gmail.com>
2017-04-16 21:08:34 -0400

    Added to kbd and window bringer

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,12 +3,19 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe, hPutStrLn)
 import XMonad.Util.EZConfig(additionalKeys, removeKeys)
+import XMonad.Actions.WindowBringer
+
+myBringer = WindowBringerConfig
+            { menuCommand  = "dmenu"
+            , menuArgs     = ["-b", "-fn", "\"Ubuntu\""]
+            , windowTitler = windowTitler (def :: WindowBringerConfig)
+            }
 
 main = do
     xmproc <- spawnPipe "xmobar ~/.config/xmobar/xmobarrc"
     xmonad $ docks def
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        { manageHook = manageDocks <+> manageHook def
+        , layoutHook = avoidStruts  $  layoutHook def
         , logHook    = dynamicLogWithPP $ xmobarPP {
             ppOutput = hPutStrLn xmproc
           -- , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
@@ -19,4 +26,5 @@ main = do
         , modMask    = mod4Mask
         }
         `additionalKeys`
-        [((mod4Mask, xK_p), spawn "dmenu_run -b -fn 'Ubuntu'")]
+        [((mod4Mask, xK_p), spawn "rofi -modi \"run,ssh\" -show \"run\" -font \"Ubuntu 14\""),
+         ((mod4Mask, xK_w), gotoMenuConfig myBringer)]
