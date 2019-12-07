820f3c29ce191dd3f3c48e9a9b86ef44da4702f0
Heman Gandhi <hemangandhi@gmail.com>
2019-12-07 10:21:22 -0500

    re-org: groupby program roughly

--- commit-push.sh
+++ commit-push.sh
@@ -1,40 +1,50 @@
 #! /usr/bin/sh
 
+# tmux
 cp ~/.tmux.conf .
+
+# vim
 cp ~/.vimrc .
-cp ~/.inputrc .
-cp ~/.zshrc .
-cp ~/.xprofile .
-cp ~/.xinitrc .
-cp ~/.nvidia-xinitrc .
-cp ~/.Xresources .
-cp ~/.Xmodmap .
-
-cp ~/.config/compton.conf .config/
-cp ~/.fehbg .
-
-cp -r ~/.ssh .
-
 cp -r ~/.vim/ftplugin .
 cp -r ~/.vim/colors .
 
+# shell
+cp ~/.inputrc .
+cp ~/.zshrc .
 cp -r ~/.bashrc .
 cp -r ~/.bash_prompt .
 cp -r ~/.bash_git .
 cp -r ~/.bash_aliases .
 
+# X
+cp ~/.xprofile .
+cp ~/.xinitrc .
+cp ~/.nvidia-xinitrc .
+cp ~/.Xresources .
+
+# keyboard
+cp ~/.Xmodmap .
+cp ~/trail.kmap .
+
+# compton and backgrounds
+cp ~/.config/compton.conf .config/
+cp ~/.fehbg .
+cp -r ~/.config/backgrounds .config/
+
+# emacs
 cp ~/.emacs.d/init.el .
 
-cp -r ~/.config/sway .config/
-cp -r ~/.config/xmobar .config/
-cp -r ~/.config/backgrounds .config/
+# zathura
 cp -r ~/.config/zathura .config/
+
+# i3
 cp -r ~/.config/i3 .config/
 cp -r ~/.config/i3status .config/
+
+# Neovim
 cp ~/.config/nvim/init.vim .config/nvim/init.vim
 
-cat ~/.muttrc | grep -v pass | grep -v spoolfile > .muttrc
-
+# Yeet into git
 git add -A
 git commit -m "$1"
 git push
