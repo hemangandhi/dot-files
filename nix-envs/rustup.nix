04fd19e7b2f5c2616629c331e86a90c4dfc63fa2
hemangandhi <hemangandhi@gmail.com>
2021-05-15 15:53:19 -0400

    better nightly rust env

--- nix-envs/rustup.nix
+++ nix-envs/rustup.nix
@@ -1,25 +1,28 @@
 # Difference between this an rust.nix?
-# This uses rustup so that we can switch Rust versions
+# This uses overlays so that we can switch Rust versions
 # for experimenting with nightly etc.
+# It's probably better to use this with a rust toolchain manifest
+# too so that we can have reproducible builds through rustup
+# or nix.
 
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
+let
+  rust_overlay = import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rust_of_choice = nixpkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
+in
+nixpkgs.stdenv.mkDerivation {
   name = "rustup-env";
   buildInputs = [
     # editor
-    emacs26Packages.company
-    emacs26Packages.rustic
-    emacs26Packages.flycheck
-    emacs26Packages.eglot
-    emacs26Packages.cargo
-    # linter
-    clippy
-    # builder
-    cargo
-    # compiler
-    rustc
-    # formatter
-    rustfmt
+    nixpkgs.emacs26Packages.company
+    nixpkgs.emacs26Packages.rustic
+    nixpkgs.emacs26Packages.flycheck
+    nixpkgs.emacs26Packages.flycheck-rust
+    nixpkgs.emacs26Packages.flymake
+    nixpkgs.emacs26Packages.flymake-rust
+    nixpkgs.emacs26Packages.eglot
+    nixpkgs.emacs26Packages.cargo
+    # rust
+    rust_of_choice
   ];
 }
