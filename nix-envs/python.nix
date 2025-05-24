5d2be3d70afee12353749ecc2730896916aca2e5
Heman Gandhi <hemangandhi@gmail.com>
2025-05-25 01:10:45 +0900

    Make Python LSP work

--- nix-envs/python.nix
+++ nix-envs/python.nix
@@ -1,12 +1,27 @@
-# Nix packages version of "the set of things you need in your system to code
-# this"
-
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
-  name = "sukuraberu-dev-env";
+let
+  nixpkgs = import <nixpkgs> {};
+  nvim = import ./nvim.nix ( rec {
+    nixPkgs = nixpkgs;
+    neovim = nixpkgs.neovim;
+    customPackages = with nixpkgs.vimPlugins; {
+      start = [
+        coc-pyright
+      ];
+    };
+  }); in
+nixpkgs.stdenv.mkDerivation {
+  name = "py-env";
   buildInputs = [
-    # Python
-    python3
+    # Python (arbitrarily picking a version)
+    nixpkgs.python311
+    nixpkgs.python311Packages.python-lsp-server
+    nixpkgs.python311Packages.mypy
+    nixpkgs.python311Packages.ruff
+    # Package management (within Python)
+    nixpkgs.python311Packages.pip
+    # Editor
+    nvim
+    # coc needs this
+    nixpkgs.nodejs
   ];
 }
