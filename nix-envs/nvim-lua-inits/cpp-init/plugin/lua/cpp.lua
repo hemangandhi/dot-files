fb492ca2481a12416175433c0c94b6b64b798dc3
Heman Gandhi <hemangandhi@gmail.com>
2025-05-25 01:10:30 +0900

    Make C++ LSP work

---
+++ nix-envs/nvim-lua-inits/cpp-init/plugin/lua/cpp.lua
@@ -0,0 +1,6 @@
+local lspconfig = require('lspconfig')
+
+lspconfig.clangd.setup({
+    cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+    init_options = { fallbackFlags = { '-std=c++17' }, },
+})
