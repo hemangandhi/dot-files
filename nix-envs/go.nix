136324295560851f080fc322674f398947e51bfc
Heman Gandhi <hemangandhi@gmail.com>
2025-07-05 12:37:23 +0900

    Use helix for golang

--- nix-envs/go.nix
+++ nix-envs/go.nix
@@ -1,20 +1,14 @@
 let
   nixpkgs = import <nixpkgs> {};
-  nvim = import ./nvim.nix ( rec {
-    nixPkgs = nixpkgs;
-    neovim = nixpkgs.neovim;
-    customPackages = with nixpkgs.vimPlugins; {
-      start = [
-        go-nvim
-      ];
-    };
-  }); in
+in
 nixpkgs.stdenv.mkDerivation {
   name = "golang-env";
   buildInputs = [
     # editor
-    nvim
+    nixpkgs.helix
     # PL
     nixpkgs.go
+    # LSP
+    nixpkgs.gopls
   ];
 }
