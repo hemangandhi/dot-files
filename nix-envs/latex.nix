c8f40ede5137de84422eb2f999dbcc479f741221
Heman Gandhi <hemangandhi@gmail.com>
2025-12-05 22:09:31 +0900

    feat: match the Lean's LaTex config

--- nix-envs/latex.nix
+++ nix-envs/latex.nix
@@ -1,12 +1,31 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
+let
+  nixpkgs = import <nixpkgs> {};
+  nvim = import ../../nvim.nix ( rec {
+    nixPkgs = nixpkgs;
+    neovim = nixpkgs.neovim;
+    customPackages = with nixpkgs.vimPlugins; {
+      start = [
+        vimtex
+      ];
+    };
+    extraRC = ''
+      " VimTex confs
+      let g:vimtex_view_method = 'zathura'
+      let g:vimtex_compiler_method = 'tectonic'
+    '';
+  }); in
+nixpkgs.stdenv.mkDerivation {
   name = "latex-env";
   buildInputs = [
     # editor
-    emacs.pkgs.elpaPackages.company
-    emacs.pkgs.elpaPackages.auctex
-    # Latex
-    
+    nvim
+    nixpkgs.nodejs
+    # For vimtex
+    nixpkgs.tectonic
+    # PL
+    nixpkgs.texliveTeTeX
+    # convenience
+    nixpkgs.xclip
+    nixpkgs.zathura
   ];
 }
