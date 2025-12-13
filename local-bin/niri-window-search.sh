95fdba23f9832ea0f21bda99fedcd9269b065573
Heman Gandhi <hemangandhi@gmail.com>
2025-12-13 15:54:00 +0900

    feat: fuzzel search to go to a window

---
+++ local-bin/niri-window-search.sh
@@ -0,0 +1,6 @@
+# Depends on niri, fuzzel, jq, and some normal Linux tools.
+
+ID_COMMA_TITLE=$(niri msg -j windows | jq -r '.[] | [.id, .title] | @csv' | fuzzel --dmenu | cut -d, -f1)
+if [ ! -z "$ID_COMMA_TITLE" ]; then
+    niri msg action focus-window --id "$ID_COMMA_TITLE"
+fi
